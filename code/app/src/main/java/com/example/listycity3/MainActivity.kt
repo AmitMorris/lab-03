145ecd28f680dbbf55366874b1cbb7de0a0c4527
Melody Morris <amitabh1@ualberta.ca>
2026-09-16 18:27:11 -0600

    removed an unnessesary comment

--- code/app/src/main/java/com/example/listycity3/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity3/MainActivity.kt
@@ -40,26 +40,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-//class CityRepository {
-//    private val _cities = mutableStateListOf(
-//        City("Edmonton", "AB"),
-//        City("Vancouver", "BC"),
-//        City("Toronto", "ON")
-//    )
-//
-//
-//    val cities: List<City>
-//        get() = _cities
-//
-//    fun addCity(city: City) {
-//        _cities.add(city)
-//    }
-//
-//    fun editCity(oldCity: City, newCity: City) {
-//        val _index = _cities.indexOf(oldCity)
-//        if (_index != -1) {
-//            _cities[_index] = newCity
-//        }
-//    }
-//}
